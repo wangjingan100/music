1b0b6907b9d980985ac1b2ffd8c33e3ca032d286
wja <1258399119@qq.com>
2020-12-14 22:53:12 +0800

    添加头部和导航

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
   <div id="app">
-    <div class="test"></div>
-    <p>我是段落</p>
+    <Header></Header>
+    <Tabbar></Tabbar>
+    <router-view></router-view>
+    <!--
+      改为点击头部换皮肤了
+      <div class="theme">
+      <button @click="myFn('theme')"></button>
+      <button @click="myFn('theme1')"></button>
+      <button @click="myFn('theme2')"></button>
+    </div> -->
   </div>
 </template>
 
-<style lang="scss">
-.test{
-  width: 200Px;
-  height: 200Px;
-  background: red;
-  // transform: translate(100px,100px);
+<script>
+import Header from './components/Header'
+import Tabbar from './components/Tabbar'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tabbar
+  },
+  methods: {
+    myFn (data) {
+      // console.log(data)
+      // document.documentElement.setAttribute('data-theme', data)
+    }
+  }
 }
+</script>
+<style lang="scss" scoped>
+@import 'assets/css/variable.scss';
+.theme{
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  button{
+    width: 100px;
+    height: 100px;
+    &:nth-of-type(1){
+      background: $background-color-theme;
+    }
+    &:nth-of-type(2){
+      background: $background-color-theme1;
+    }
+    &:nth-of-type(3){
+      background: $background-color-theme2;
+    }
+  }
+}
+
 </style>
---
+++ src/views/Recommend.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="recommend">
+    热门推荐
+  </div>
+</template>
+<script>
+export default {
+  name: 'Recommend'
+}
+</script>
+<style scoped>
+
+</style>
